889a73445ed01aeba9049db1579dc2e59d71bc24
Maya <maya.arguell@gmail.com>
2018-04-17 14:29:47 -0400

    Connected first drawing to backend, tweaked drawing algorithm

--- Development/js/index.js
+++
@@ -1,8 +0,0 @@
-$('.toolBar>ul>li').click(function() {
-    if ($(this).attr('class') == 'active') {
-        $(this).removeClass('active');
-    } else {
-        $('.active').removeClass('active');
-        $(this).addClass('active');
-    }
-});
